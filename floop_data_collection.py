4dc66a8f82319cf1394a67734ff84fc51dc6aa0a
Dale Berg <dale.brg@gmail.com>
2022-01-20 20:44:38 -0800

    clean up some rough edges

--- floop_data_collection.py
+++ floop_data_collection.py
@@ -1,26 +1,35 @@
 import firebase_admin
 from firebase_admin import firestore
 import json
+from os.path import exists
 
-#initialize connection, auth
-def initialize_connection():
-    path_to_cert = input("input path to firebase cert file:")
-    cred_obj = firebase_admin.credentials.Certificate(path_to_cert)
+def get_credentials():
+    cert_path = input('enter path to cert file: ')
+    while not exists(cert_path):
+        cert_path = input('invalid file path. Please enter path to Floop cert file: ')
+    return cert_path
+
+def initalize_connection(cert_path):
+    cred_obj = firebase_admin.credentials.Certificate(cert_path)
+    # initialize default app for firebase
     firebase_admin.initialize_app(cred_obj)
     db = firestore.client()
-    return db.collection(u'Databases/Dev_Database/Conversations').list_documents()
+    # exclude certain types of comment, on Floop's suggestion
+    return db.collection(u'Databases/Dev_Database/Conversations').where('Comment_Preview', 'not-in', [
+        'What is your goal for this year?', 'Audio Comment', 'Freeform', 'Freeform Comment', '', ' ']).limit(
+        1000).stream()
 
 # descend into the tree of document -> collection -> document and pull out salient text
 if __name__ == '__main__':
-    s = set()
-    for convo_doc in initialize_connection():
-        all_messages = convo_doc.collection("Messages").list_documents()
-        if len(s) > 1000:
-            break
-        for message_doc in all_messages:
-            s.add(message_doc.get().to_dict()["Text"])
-            if len(s) > 1000:
-                break
+    arr = []
+    cert_path = get_credentials()
+    conversation_documents = initalize_connection(cert_path)
+    for convo_doc in conversation_documents:
+        all_messages = convo_doc.reference.collection('Messages').order_by(u'Date_Submitted').limit(1).get()
+        message_doc = all_messages[0]
+        arr.append(message_doc._data["Text"])
+        # leaving this print statement in because the operation is so long running, it's helpful to
+        # prove that the script isn't stuck
+        print(arr)
     with open('floop_data.json', 'w') as f:
-        json.dump(s, f)
-
+        json.dump(list(set(arr)), f)
