54d5054fa74ee5f3524ac618bc5fbdfdd5e4bef1
Turki BK <tbkaily.gmail.com>
2022-01-23 12:54:13 -0800

    add a change

--- backend/simpleLambda/index.js
+++ backend/simpleLambda/index.js
@@ -2,11 +2,19 @@ exports.handler = async (event, context, callback) => {
   // TODO implement
   console.log(event);
   const response = {
+    metood: "post",
     statusCode: 200,
     headers: {
-        'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify("Hi!"),
+    body: JSON.stringify([
+      {
+        id: 1,
+        name: "TI",
+        description: "Hi from Lambda! ID# 1",
+      },
+      { id: 2, name: "Kl", description: "Hi from Lambda! ID# 2" },
+    ]),
   };
   //return response;
   callback(null, response);
