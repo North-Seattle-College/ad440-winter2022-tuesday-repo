09cf17633838f6bc52b283429b1fa251940ad2e4
dsjack <dsjack89@gmail.com>
2022-02-02 19:37:22 -0800

    feat(makeReactBucket.sh): make script callable from anywhere

--- devops/makeReactBucket/makeReactBucket.sh
+++ devops/makeReactBucket/makeReactBucket.sh
@@ -4,7 +4,8 @@ MIN=2
 MAX=5
 RANDOM_STRING=$(cat /dev/urandom | base64 | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
 INITIALS_PROMPT="What are your initials? ('q' to quit)"
-BUCKET_TEMPLATE="./makeReactBucket.json"
+SCRIPT_PATH=`dirname $0`
+BUCKET_TEMPLATE="$SCRIPT_PATH/makeReactBucket.json"
 
 initials=$1
 skipPrompt=false
@@ -33,6 +34,7 @@ while :
         done
     else
       skipPrompt=true
+      answer=y
     fi
 
     if [ "$initials" == "q" ]; then
@@ -54,10 +56,9 @@ while :
               "    - stack: $stackName" \
               "Create bucket and stack? (y/n)" >&1
       printf "%s" "> " >&1
-      printf "%s" "$skipPrompt" >&1
       while read answer && [ "$answer" != "q" ];
         do
-          if [ "${answer:0:1}" == "y" || "${answer:0:1}" == "n" ]; then
+          if [ "${answer:0:1}" == "y" ] || [ "${answer:0:1}" == "n" ]; then
             break
           else
             printf "%s\n" "Create bucket? (y/n)" >&1
@@ -66,9 +67,9 @@ while :
         done
     fi
 
-    if [ "${answer:0:1}" == "y" || "$skipPrompt" = true ]; then
+    if [ "${answer:0:1}" == "y" ]; then
       printf '%s\n' "aws cloudformation deploy --template-file ./makeReactBucket.json --stack-name $stackName --parameter-overrides BucketName=$bucketName" >&1
-      aws cloudformation deploy --template-file $BUCKET_TEMPLATE --stack-name $stackName --parameter-overrides BucketName=$bucketName
+      aws cloudformation deploy --template-file "$BUCKET_TEMPLATE" --stack-name $stackName --parameter-overrides BucketName=$bucketName
     fi
     printf '%s\n' "Done" >&1
     break
