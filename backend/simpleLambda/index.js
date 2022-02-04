fa21dc23852d5c2df255769ae020c0e41fb79b7f
Turki BK <tbkaily.gmail.com>
2022-02-03 16:52:44 -0800

    fix buges and hunddle the body

--- backend/simpleLambda/index.js
+++ backend/simpleLambda/index.js
@@ -1,25 +1,55 @@
-//let mes= event["hi"];
 exports.handler = async (event, context, callback) => {
   // TODO implement
-  console.log("----- lambda start");
   console.log(event);
+  console.log("----- lambda start");
 
+  //createMessage(event);
   console.log("start if");
-  if (event.httpMethod === "post") {
-    console.log(event);
+  if (event.body) {
+    //  console.log(event);
     return createMessage(event);
+  } else {
+    return {
+      statusCode: 400,
+    };
   }
 };
 console.log("createMessage start");
 function createMessage(event) {
   // body...
+  // console.log("inside method");
   const body = JSON.parse(event.body);
-  const response = {
-    statusCode: 201,
-    body: JSON.stringify({
-      message: "succesfuly",
-      details: body,
-    }),
-  };
-  return response;
+  if (body.name === "")
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Unsuccesfuly,empty string",
+        details: body,
+      }),
+    };
+  else if (body.name === "ping")
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        message: "succesfuly, pong",
+        details: body,
+      }),
+    };
+  else if (body.name !== "ping" && body.name !== "")
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Unsuccesfuly,not correct data",
+        details: body,
+      }),
+    };
+
+  //   const response = {
+  //     statusCode: 201,
+  //     body: JSON.stringify({
+  //       message: "succesfuly",
+  //       details: body
+  //     })
+  //   };
+  // return response;
 }
