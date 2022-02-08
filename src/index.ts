32339c7d105483ed85be9abdc11bbf9dbdbc0d8f
Annelisebx <50610153+Annelisebx@users.noreply.github.com>
2022-02-08 14:24:59 -0800

    removing files

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export const handler = async (): Promise<any> => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify('Hello from AWS Lambda Function!'),
-    };
-    return response;
-  };
