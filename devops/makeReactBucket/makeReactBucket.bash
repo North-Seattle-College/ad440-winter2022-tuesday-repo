575e45764a0db1ed4973eac3a1ab4ea227b0779c
Dan <dsjack89@gmail.com>
2022-02-08 13:35:27 -0800

    Make S3 bucket script machine runnable (#60)
    * feat(makeReactBucket.sh): bypass user input with shell arg
    
    * feat(makeReactBucket.sh): make script callable from anywhere
    
    * chore(makeReactBucket.sh): make quotes convention more consistent
    
    * feat(makeReactBucket.sh): extract initials validation function
    
    * refactor(/devops): moved function to a helper file
    
    * refactor(/devops): make function take arg instead of use global
    
    * chore(/devops): lint, change from .sh to .bash
    
    * refactor(makeReactBucket.json): converted to yml for consistency with other template files
    
    * chore(makeReactBucket.json): delete file
    
    * Merge development into dj-40
    
    commit 875b6bc7c3ad79faa44dcf9376450949e1271daa
    Author: Daniel <56328015+eyerusalemdani@users.noreply.github.com>
    Date:   Thu Feb 3 16:02:49 2022 -0800
    
        Python script run feedback datasets#22 (#30)
    
        * first
    
        * update on assignment
    
        * Modified task.py
    
        * deleted floop_data_15k.json and word2vec11.model
    
        * removed the commented
    
        * word2vec11.model file added
    
        * updated
    
        * update line 83
    
        * update csv file
    
        * updates of
    
        * Update on the code
    
        * floop_data_15k.json deleted
    
        * DataFromS3 to Field1 changed
    
        * line 25 added
    
        * Update on task.py and added info.md file
    
    commit 4880bf779d6058886b6c03603b27c4a411d5e706
    Merge: d0940f8 8c0b991
    Author: ChrisNavo <52547964+ChrisNavoczynski@users.noreply.github.com>
    Date:   Thu Feb 3 14:11:15 2022 -0800
    
        Merge pull request #59 from North-Seattle-College/react-S3-action
    
        React S3 Action deploy
    
    commit 8c0b991c769c45db2d9e8a841bde6d1c5f7069c1
    Author: CNavo <chrisnavo01@gmail.com>
    Date:   Wed Feb 2 21:50:37 2022 -0800
    
        React S3 Action deploy

---
+++ devops/makeReactBucket/makeReactBucket.bash
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname "$0")
+
+# import validateInitials from common.lib
+# shellcheck disable=SC1091
+. "$SCRIPT_PATH/../common.lib"
+
+RANDOM_STRING=$(cat /dev/urandom | base64 | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
+BUCKET_TEMPLATE="$SCRIPT_PATH/makeReactBucket.yml"
+INITIALS_PROMPT="What are your initials? ('q' to quit)"
+
+initials="$1"
+skipPrompt=false
+
+while :; do
+  # get and validate user initials if not passed as arg
+  if [[ -z "$initials" ]]; then
+    printf "%s\n" "$INITIALS_PROMPT" >&1
+    printf "%s" "> " >&1
+    while read -r initials && [ "$initials" != "q" ]; do
+      if validateInitials "$initials"; then
+        break
+      fi
+      printf "%s\n" "$INITIALS_PROMPT" >&1
+      printf "%s" "> " >&1
+    done
+  elif validateInitials "$initials"; then
+    skipPrompt=true
+    answer=y
+  else
+    break
+  fi
+
+  if [ "$initials" == "q" ]; then
+    printf '%s\n' "Done" >&1
+    break
+  fi
+
+  formattedInitials="$(echo "${initials}" | tr -d '[:space:]' | tr '[:upper:]' '[:lower:]')"
+  todayDate=$(date +'%Y%m%d')
+  uniqueName=$formattedInitials-$RANDOM_STRING-$todayDate
+
+  bucketName=$uniqueName-bucket
+  stackName=$uniqueName-stack
+
+  if [ "$skipPrompt" = false ]; then
+    # show user name of bucket and stack, get confirmation to create resources
+    printf "%s\n" "Your bucket and stack will be named as follows:" \
+      "    - bucket: $bucketName" \
+      "    - stack: $stackName" \
+      "Create bucket and stack? (y/n)" >&1
+    printf "%s" "> " >&1
+    while read -r answer && [ "$answer" != "q" ]; do
+      if [ "${answer:0:1}" == "y" ] || [ "${answer:0:1}" == "n" ]; then
+        break
+      else
+        printf "%s\n" "Create bucket? (y/n)" >&1
+        printf "%s" "> " >&1
+      fi
+    done
+  fi
+
+  if [ "${answer:0:1}" == "y" ]; then
+    printf '%s\n' "aws cloudformation deploy --template-file $BUCKET_TEMPLATE --stack-name $stackName --parameter-overrides BucketName=$bucketName" >&1
+    aws cloudformation deploy --template-file "$BUCKET_TEMPLATE" --stack-name "$stackName" --parameter-overrides BucketName="$bucketName"
+  fi
+  printf '%s\n' "Done" >&1
+  break
+done
