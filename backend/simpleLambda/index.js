286989e8a93a5b493ff159d4cd8705cfa99f83ee
Turki BK <tbkaily.gmail.com>
2022-02-08 18:42:09 -0800

    improve the lambda to accept teacher feed back

--- backend/simpleLambda/index.js
+++ backend/simpleLambda/index.js
@@ -1,51 +1,35 @@
-const messafe='null'
+exports.handler = async (event) => {
+  console.log("event: " + JSON.stringify(event));
+  console.log("----- lambda start");
+  let feedBack = "Good Job!";
+  // let Id = 1;
 
-exports.handler = async (event, context, callback) => {
-  // TODO implement
-  console.log(event);
-  const response = {
-    metood: "post",
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(event[body
-      // {
-      //   id: 1,
-      //   name: "TI",
-      //   description: "Hi from Lambda! ID# 1",
-      // },
-      // { id: 2, name: "Kl", description: "Hi from Lambda! ID# 2" },
-    ]),
-    //const message
-    messafe = body["massafe"]
+  let responseCode = 201;
+  console.log("event: " + JSON.stringify(event));
+
+  if (event.body) {
+    let body = JSON.parse(event.body);
+    if (body.feedBack === "")
+      return {
+        statusCode: 400,
+      };
+    else feedBack = body.feedBack;
+    // if (body.Id)
+    // Id = body.id;
+  }
+
+  let FeedBack = `Teacher feed back:  ${feedBack}.`;
+
+  let responseBody = {
+    message: FeedBack,
+    // input: event
   };
-  //return response;//let mes= event["hi"];
-exports.handler = async (event, context, callback) => {
-  // TODO implement
-  console.log("----- lambda start")
-  console.log(event);
- 
-   console.log("start if");
-if(event.httpMethod === "post"){
-     console.log(event);
-   return createMessage(event);
- }
- 
-};
-  console.log("createMessage start");
-function createMessage(event) {
-  // body...
- const body = JSON.parse(event.body);
- const response = {
-    
-    statusCode: 201,
-    body:JSON.stringify({
-      message:"succesfuly",
-      details: body
-    })
- };
- return response;
-}
-  callback(null, response);
+
+  let response = {
+    statusCode: responseCode,
+
+    body: JSON.stringify(responseBody),
+  };
+
+  return response;
 };
