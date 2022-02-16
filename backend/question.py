145cd98a953b9deb6bc554b49d1b8ae94905ebcf
AGT1041 <abety8@outlook.com>
2022-02-15 23:11:00 -0800

    updated response

--- backend/question.py
+++ backend/question.py
@@ -1,8 +1,8 @@
 import nltk
 from nltk.corpus import nps_chat
  
-Question_Words = [ 'where', 'when','how',"couldn't",'why',"isn't",'did','do', 'have','am','is','are','can','could','may','would','should'
-"didn't","doesn't","haven't","aren't", "can't","wouldn't","won't","shouldn't",'?']
+Question_Words = [ 'where','how','why','did','do','does',"isn't",'has','am i', 'are','can','could','is','may',"can't", 
+"didn't",'will','when',"doesn't","haven't",'have','what',"aren't",'would',"couldn't","wouldn't","won't","shouldn't",'should']
  
 posts = nltk.corpus.nps_chat.xml_posts()[:10000]
  
@@ -12,7 +12,7 @@ sent=input("Inter sentence: ")
     # 2. Tokenize sentences using NLTK's word_tokenize
     # return dict of tokenized words
 
-def dialogue_act_features(post):
+def post_features(post):
     features = {}
     for word in nltk.word_tokenize(post):
         features['contains({})'.format(word.lower())] = True
@@ -27,11 +27,15 @@ def dialogue_act_features(post):
     # Return: Classifier object
 
 def __perform_classification():
-        featuresets = [(dialogue_act_features(post.text), post.get('class')) for post in posts]
+        featuresets = [(post_features(post.text), post.get('class')) for post in posts]
+         
+        #print(f)
         training_size = int(len(featuresets) * 0.1)
         train_set, test_set = featuresets[training_size:], featuresets[:training_size]
+         
         classifier = nltk.NaiveBayesClassifier.train(train_set)
          
+        #$print(classifier)
         return classifier
  
  
@@ -42,29 +46,32 @@ cl= __perform_classification()
 types = ["whQuestion","ynQuestion","Statement"]
 # Input a sentence
 # returns the type sentence
-def is_ques_using_nltk(ques):
-    question_type = cl.classify(dialogue_act_features(ques)) 
+def is_ques(ques):
+    question_type = cl.classify(post_features(ques)) 
      
     return  question_type
-
+    
  
 
     # Method : IsQuestion
     # Input: Sentence to be predicted
     # Return: type of sentence it is using nltk  
 def IsQuestion(sentence):
-    type=is_ques_using_nltk(sentence)
+    type=is_ques(sentence)
     #print("type is"+type) 
-       
+    first_word = sentence.split()[0].lower()  
+     
     
-    for pattern in Question_Words:
-        if pattern in sentence.split()[0] or  type=="whQuestion" or type== "ynQuestion"  :
-            print("This is a question")
-            exit()                      
+    if  ((type=="whQuestion" or type== "ynQuestion") or (first_word in Question_Words ))  :
              
+        print("This is a question")
+                                 
+    elif type=="Other":
+         print("This is garbled text")   
+
     else:        
         print("This is a statement") 
          
-question="  are you smart"   
+  
 IsQuestion(sent)
  
