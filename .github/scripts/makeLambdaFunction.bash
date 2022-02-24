828a5c60c3079e8e029430dc65e2a372e0e0b0ce
dsjack <dsjack89@gmail.com>
2022-02-23 16:01:36 -0800

    test

--- .github/scripts/makeLambdaFunction.bash
+++ .github/scripts/makeLambdaFunction.bash
@@ -1,8 +1,8 @@
 # !/bin/bash
 INITIALS_PROMPT="Please enter your first and last initial (or 'q' to quit)"
 NUM=2
-initials=$1
-
+initials="$1"
+echo "$initials"
 while :
   do
     # get and validate user initials
@@ -30,6 +30,7 @@ while :
     
     # converts initials to lowercase and remove spaces
     formattedInitials="$(echo "${initials}" | tr -d '[:space:]' | tr '[:upper:]' '[:lower:]')"
+    echo "$formattedInitials"
     todayDate=$(date +'%Y%m%d')
     OPENSSL_STRING=$(openssl rand -hex 3)
     RANDOM_STRING=${OPENSSL_STRING:0:5}
@@ -46,20 +47,20 @@ while :
 
     lambdaTemplate=".github/scripts/makeLambdaFunction.yml"
 
-    aws cloudformation deploy --region us-west-2 --template-file $lambdaTemplate --stack-name 'tempName' --parameter-overrides LambdaName=$lambdaName --capabilities CAPABILITY_NAMED_IAM
+    # aws cloudformation deploy --region us-west-2 --template-file $lambdaTemplate --stack-name 'tempName' --parameter-overrides LambdaName=$lambdaName --capabilities CAPABILITY_NAMED_IAM
    
-    while read answer && [ "$answer" != "q" ];
-      do
-        if [ "$answer" == "y" ]; then
-          printf '%s\n' "aws cloudformation deploy --template-file $lambdaTemplate --stack-name $stackName --parameter-overrides LambdaName=$lambdaName --capabilities CAPABILITY_NAMED_IAM"
-          aws cloudformation deploy --template-file $lambdaTemplate --stack-name $stackName --parameter-overrides LambdaName=$lambdaName --capabilities CAPABILITY_NAMED_IAM
-          break
-        elif [ "$answer" == "n" ]; then
-          break
-        else
-          printf "%s\n" "Create lambda? (y/n)"
-        fi
-      done
+    # while read answer && [ "$answer" != "q" ];
+    #   do
+    #     if [ "$answer" == "y" ]; then
+    #       printf '%s\n' "aws cloudformation deploy --template-file $lambdaTemplate --stack-name $stackName --parameter-overrides LambdaName=$lambdaName --capabilities CAPABILITY_NAMED_IAM"
+    #       aws cloudformation deploy --template-file $lambdaTemplate --stack-name $stackName --parameter-overrides LambdaName=$lambdaName --capabilities CAPABILITY_NAMED_IAM
+    #       break
+    #     elif [ "$answer" == "n" ]; then
+    #       break
+    #     else
+    #       printf "%s\n" "Create lambda? (y/n)"
+    #     fi
+    #   done
     printf '%s\n' "Done" 
     break
   done 
